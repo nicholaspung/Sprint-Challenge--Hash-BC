a2d29970e6eaedb48d2b819e593d079c95bfa90d
Nicholas Pung <nckpng95@gmail.com>
2019-11-01 10:22:22 -0700

    Finished Hashtables Ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,10 +8,35 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
+    # weights = []
+    # length = int
+    # limit = int
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+        if hash_table_retrieve(ht, weights[i]):
+            hash_table_retrieve(ht, weights[i])['quantity'] += 1
+            hash_table_retrieve(ht, weights[i])['index'] = i
+        else:
+            hash_table_insert(ht, weights[i], { 'quantity': 1, 'index': i })
+        if not hash_table_retrieve(ht, limit - weights[i]):
+            hash_table_insert(ht, limit - weights[i], { 'quantity': 0 })
+
+    for i in range(length):
+        difference = limit - weights[i]
+        instances = 1
+        if difference == weights[i]:
+            instances = 2
+
+        pair = hash_table_retrieve(ht, difference)
+        if pair and pair['quantity'] == instances:
+            if i > pair['index']:
+                print(i, pair['index'])
+                return (i, pair['index'])
+            else:
+                print(pair['index'], i)
+                return (pair['index'], i)
 
     return None
 
